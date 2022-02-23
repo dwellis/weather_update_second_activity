f729d4ac470021c1f1c5cfc9d4c9380b8a82e358
John <jmitropoulos@alumni.berklee.edu>
2022-02-22 19:19:33 -0600

    updated MainActivity and implemented volley in gradle file

--- app/src/main/java/android/example/weatherupdate/MainActivity.java
+++ app/src/main/java/android/example/weatherupdate/MainActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import java.text.DecimalFormat;
 
@@ -26,5 +32,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWeatherDetails(View view) {
+        String tempUrl = "";
+        String city = etCity.getText().toString().trim();
+        String country = etCountry.getText().toString().trim();
+
+        if (city.equals("")) {
+            tvResult.setText("Must choose a city");
+        } else {
+            if(!country.equals("")) {
+                tempUrl = url + "?q=" + city + "," + country + "&appid=" + appid;
+            } else {
+                tempUrl = url + "?q=" + city + "&appid=" + appid;
+            }
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
