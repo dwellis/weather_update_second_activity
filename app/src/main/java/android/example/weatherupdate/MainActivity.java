ef708a1384902233a868d5b22d493052054abde3
John <jmitropoulos@alumni.berklee.edu>
2022-02-22 18:54:17 -0600

    Added API key and initialization for views in MA

--- app/src/main/java/android/example/weatherupdate/MainActivity.java
+++ app/src/main/java/android/example/weatherupdate/MainActivity.java
@@ -3,12 +3,28 @@ package android.example.weatherupdate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etCity, etCountry;
+    TextView tvResult;
+    private final String url = "http://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "1b79bf73d3b4001b6fbc49847ec98423";
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCity = findViewById(R.id.etCity);
+        etCountry = findViewById(R.id.etCountry);
+        tvResult = findViewById(R.id.tvResult);
+    }
+
+    public void getWeatherDetails(View view) {
     }
 }
